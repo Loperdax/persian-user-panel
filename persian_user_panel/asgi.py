5cd9b949c2305802f64a7a74de4c21ed22155c8b
loperdax <loperdax@gmail.com>
2023-05-07 17:55:11 +0430

    MyUser and MyUserManager created

--- persian_user_panel/asgi.py
+++ persian_user_panel/asgi.py
@@ -1,11 +1,3 @@
-"""
-ASGI config for persian_user_panel project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
 
 import os
 
